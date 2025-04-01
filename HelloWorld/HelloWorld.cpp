f688059efe22cb87292244024cb43c3a0643d728
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-04-01 13:31:15 -0600

    added git message to main

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
+    //HelloWorld, Git has been added to this project.
     cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
     return 0;
 }
