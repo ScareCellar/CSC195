b063cd2585d56cc193cd02db73ec742941f2e365
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-04-08 14:07:41 -0600

    coded all of the random number generator

--- NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame.cpp
@@ -1,8 +1,36 @@
 // NumberGuessingGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
+#include <random>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World";
+	int numberOfGuesses = 3;
+	cout << "Hello World" << endl;
+	random_device rd;
+	mt19937 gen(rd());		
+	uniform_int_distribution<> distrib(1, 100);
+
+	int randomNumber = distrib(gen);
+
+	while (numberOfGuesses > 0)
+	{
+		
+		int guessedNumber;
+		cin >> guessedNumber;
+		cout << guessedNumber << endl;
+
+		if (guessedNumber == randomNumber)
+		{
+			cout << "You were correct, congratulations" << endl;
+			numberOfGuesses = 0;
+		}
+		else
+		{
+			numberOfGuesses--;
+			cout << "I'm sorry, that's not right, you have " << numberOfGuesses << " guesses left." << endl;
+		}
+	}
+	cout << "Thank you for playing";
 }
